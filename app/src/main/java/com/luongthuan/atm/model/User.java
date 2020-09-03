0ddff8b1ccc27a2e159369454546f5402f22a5af
luongthuan-lv <luongthuan2k@gmail.com>
2020-09-03 11:36:29 +0700

    Initial commit

---
+++ app/src/main/java/com/luongthuan/atm/model/User.java
@@ -0,0 +1,21 @@
+package com.luongthuan.atm.model;
+
+public class User {
+    String title,hint;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+}
