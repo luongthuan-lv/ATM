239ef288c78d5365dd5df9efa954ab9f025a3d76
luongthuan-lv <luongthuan2k@gmail.com>
2020-09-04 14:14:14 +0700

    fix customview

--- app/src/main/java/com/luongthuan/atm/view/MainActivity.java
+++ app/src/main/java/com/luongthuan/atm/view/MainActivity.java
@@ -13,66 +13,20 @@ import com.luongthuan.atm.databinding.ActivityMainBinding;
 import com.luongthuan.atm.model.User;
 import com.luongthuan.atm.viewmodel.BankViewModel;
 
+import java.lang.ref.PhantomReference;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    private User user;
-    private MyClick myClick;
-
+    private CustonView custonView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //    setContentView(R.layout.activity_main);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        myClick = new MyClick(this);
-        render();
-
-
-//
-//
-//        edtNumber.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//                if (charSequence.toString().trim().length() == 0) {
-//                    btnTest.setEnabled(false);
-//                    btnTest.setBackgroundColor(Color.parseColor("#8d93ab"));
-//                    edtNumber.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-//                } else {
-//                    btnTest.setEnabled(true);
-//                    btnTest.setBackgroundColor(Color.parseColor("#40a8c4"));
-//                    edtNumber.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_account, 0);
-//                }
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {
-//
-//            }
-//        });
+        setContentView(R.layout.activity_main);
+        custonView=new CustonView(this);
 
     }
 
-    public void render() {
-        user = new User("NHẬP SỐ THẺ ATM");
-        binding.setUser(user);
-        binding.data.setButton(myClick);
-        binding.data.setViewmodel(new BankViewModel());
-    }
 
-    public class MyClick {
-        Context context;
 
-        public MyClick(Context context) {
-            this.context = context;
-        }
 
-        public void onButtonClick(User user) {
-            user.setData("123");
-            Toast.makeText(context, "Hello", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
